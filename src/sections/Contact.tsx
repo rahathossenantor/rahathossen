c5f8b7b440248525deae250d4cfc60f62d192bb3
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-12-04 16:40:39 +0600

    Contact section completed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,22 +3,32 @@ import grain from "@/assets/images/grain.jpg";
 
 const Contact = () => {
   return (
-    <div className="pt-12 pb-16">
+    <div className="pt-12 pb-16 lg:pt-20 lg:pb-24">
       <div className="container">
-        <div className="custom-gradient-bg text-gray-900 py-8 px-10 text-center rounded-xl relative overflow-hidden z-0">
+        <div className="custom-gradient-bg text-gray-900 py-8 px-10 text-center md:text-left rounded-xl relative overflow-hidden z-0">
           <div
             className="absolute inset-0 opacity-5 -z-10"
             style={{ backgroundImage: `url(${grain.src})` }}
           ></div>
-          <h2 className="font-serif text-2xl">Let&apos;s create something
-            amazing together</h2>
-          <p className="text-sm mt-2">Ready to bring your next project to life? Let&apos;s
-            connect and discuss how I can help you achieve
-            your goals.</p>
-          <button className="bg-gray-900 text-white h-12 px-6 mt-8 inline-flex items-center gap-2 rounded-xl">
-            <span className="font-semibold">Contact Me</span>
-            <ArrowUpRight className="size-4" />
-          </button>
+          <div className="flex flex-col md:flex-row items-center gap-8 md:gap-16">
+            <div>
+              <h2 className="font-serif text-2xl md:text-3xl">
+                Let&apos;s create something
+                amazing together
+              </h2>
+              <p className="text-sm md:text-base mt-2">
+                Ready to bring your next project to life? Let&apos;s
+                connect and discuss how I can help you achieve
+                your goals.
+              </p>
+            </div>
+            <div>
+              <button className="bg-gray-900 border border-gray-900 text-white h-12 px-6 inline-flex items-center gap-2 rounded-xl w-max">
+                <span className="font-semibold text-nowrap">Contact Me</span>
+                <ArrowUpRight className="size-4" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
