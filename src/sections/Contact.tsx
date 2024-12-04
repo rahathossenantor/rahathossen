f6608e91d7a9fdb816a6a3c1b531b60c1f9f3dbf
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-12-04 15:42:44 +0600

    Contact section mobile view completed

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,18 +1,23 @@
 import ArrowUpRight from "@/assets/icons/arrow-up-right.svg";
+import grain from "@/assets/images/grain.jpg";
 
 const Contact = () => {
   return (
-    <div className="py-16">
+    <div className="pt-12 pb-16">
       <div className="container">
-        <div className="custom-gradient-bg text-gray-900">
-          <h2>Let&apos;s create something
+        <div className="custom-gradient-bg text-gray-900 py-8 px-10 text-center rounded-xl relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{ backgroundImage: `url(${grain.src})` }}
+          ></div>
+          <h2 className="font-serif text-2xl">Let&apos;s create something
             amazing together</h2>
-          <p>Ready to bring your next project to life? Let&apos;s
+          <p className="text-sm mt-2">Ready to bring your next project to life? Let&apos;s
             connect and discuss how I can help you achieve
             your goals.</p>
-          <button>
-            <span>Contact Me</span>
-            <ArrowUpRight />
+          <button className="bg-gray-900 text-white h-12 px-6 mt-8 inline-flex items-center gap-2 rounded-xl">
+            <span className="font-semibold">Contact Me</span>
+            <ArrowUpRight className="size-4" />
           </button>
         </div>
       </div>
