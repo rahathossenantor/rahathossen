057cb1d654c3a68b0e59df785ac535401853d88d
Md Rahat Hossen Antor <md.rahathossenantor@gmail.com>
2024-12-05 15:29:05 +0600

    Testimonials section tablet & desktop version completed

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -47,20 +47,20 @@ const testimonials = [
 
 const Testimonials = () => {
   return (
-    <section>
+    <section className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           eyebrow="Happy Clients"
           title="What Clients Say About Me"
           description="Don't just take my word for it. See what my clients have to say about my work."
         />
-        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex flex-none gap-8">
             {
               testimonials.map((testimonial) => (
                 <Card
                   key={testimonial.id}
-                  className="max-w-xs"
+                  className="max-w-xs md:max-w-md md:p-8"
                 >
                   <div className="flex items-center gap-4">
                     <div className="size-14 rounded-full bg-gray-700 inline-flex items-center justify-center flex-shrink-0">
@@ -75,7 +75,7 @@ const Testimonials = () => {
                       <p className="text-sm text-white/40">{testimonial.position}</p>
                     </div>
                   </div>
-                  <p className="mt-4 text-sm">{testimonial.text}</p>
+                  <p className="mt-4 text-sm md:text-base md:mt-6">{testimonial.text}</p>
                 </Card>
               ))
             }
